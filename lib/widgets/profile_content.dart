9701a46d0b8747e7f98631532b9a5d7d0be2c43d
IDW <61426181+HolllD@users.noreply.github.com>
2022-06-29 16:05:01 -0300

    Página de conta

--- lib/widgets/profile_content.dart
+++ lib/widgets/profile_content.dart
@@ -1,12 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import '../constants.dart';
+
+import 'profile_list_item.dart';
 
 class ProfileContent extends StatelessWidget {
   const ProfileContent();
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Hello world!"),
+
+    var profileInfo = Expanded(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: kSpacingUnit.w * 10,
+            width: kSpacingUnit.w * 10,
+            margin: EdgeInsets.only(top: kSpacingUnit.w * 3),
+            child: Stack(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: kSpacingUnit.w * 5,
+                  backgroundImage: const NetworkImage('https://i.stack.imgur.com/JfrKO.jpg?s=192&g=1')
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Container(
+                    height: kSpacingUnit.w * 2.5,
+                    width: kSpacingUnit.w * 2.5,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).accentColor,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Center(
+                      heightFactor: kSpacingUnit.w * 1.5,
+                      widthFactor: kSpacingUnit.w * 1.5,
+                      child: Icon(
+                        LineAwesomeIcons.pen,
+                        color: kDarkPrimaryColor,
+                        size: ScreenUtil().setSp(kSpacingUnit.w * 1.5),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: kSpacingUnit.w * 2),
+          Text(
+            'Nicolas Adams',
+            style: kTitleTextStyle,
+          ),
+          SizedBox(height: kSpacingUnit.w * 0.5),
+          Text(
+            'nicolasadams@gmail.com',
+            style: kCaptionTextStyle,
+          ),
+          SizedBox(height: kSpacingUnit.w * 2),
+        ],
+      ),
+    );
+
+    var header = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(width: kSpacingUnit.w * 3),
+        profileInfo,
+        SizedBox(width: kSpacingUnit.w * 3),
+      ],
+    );
+
+
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: ListView(
+            children: <Widget>[
+              header,
+              const ProfileListItem(
+                icon: LineAwesomeIcons.user_shield,
+                text: 'Privacidade', key: null,
+              ),
+              const ProfileListItem(
+                icon: LineAwesomeIcons.question_circle,
+                text: 'Ajuda & Suporte', key: null,
+              ),
+              const ProfileListItem(
+                icon: LineAwesomeIcons.user_plus,
+                text: 'Convidar um amigo', key: null,
+              ),
+              const ProfileListItem(
+                icon: LineAwesomeIcons.alternate_sign_out,
+                text: 'Sair',
+                hasNavigation: false, key: null,
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
